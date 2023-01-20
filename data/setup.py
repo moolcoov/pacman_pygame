de2e8d2cd123341c9bbf397ec384a2cb92df2ce7
Stepanov Ilya <rukzzak@mail.ru>
2023-01-20 18:53:46 +0800

    Add icon

--- data/setup.py
+++ data/setup.py
@@ -9,4 +9,5 @@ CAPTION = const.CAPTION
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 pygame.init()
 pygame.display.set_caption(const.CAPTION)
+pygame.display.set_icon(pygame.image.load("./resources/graphics/icon.ico"))
 display = pygame.display.set_mode(const.SCREEN_SIZE)
